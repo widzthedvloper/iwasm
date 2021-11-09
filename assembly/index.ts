8785beaec63979821a4083f511b9bf01ab02c393
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-11-08 20:02:42 -0400

    Implement abort function

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,8 @@
 // The entry file of your WebAssembly module.
 
 export function add(a: i32, b: i32): i32 {
+  if (a === 1) {
+    abort();
+  }
   return a + b;
 }
--- js/loader.js
+++ js/loader.js
@@ -1,22 +1,32 @@
 class WasmLoader {
-    async wasm(path){
+    constructor(){
+        this._imports = {
+            "env": {
+                abort(){
+                    throw new Error('Abort called from wasm file');
+                }
+            }
+        }
+    }
+
+    async wasm(path, imports = this._imports){
         console.log(`Fetching ${path}`);
         if(!WebAssembly.instantiateStreaming){
-            return this.wasmFallback(path);
+            return this.wasmFallback(path, imports);
         }
 
-        const { instance } = await WebAssembly.instantiateStreaming(fetch(path));
+        const { instance } = await WebAssembly.instantiateStreaming(fetch(path), imports);
 
         return instance?.exports;
     }
 
-    async wasmFallback(path){
+    async wasmFallback(path, imports){
         console.log(`Using fallback`);
 
         const response = await fetch(path);
         const bytes = await response?.arrayBuffer();
 
-        const { instance } = WebAssembly.instantiate(bytes);
+        const { instance } = WebAssembly.instantiate(bytes, imports);
 
         return instance?.exports;
     }
